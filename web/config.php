402c0378cdd08fd196e8c89748f18628757ac107
webmasterfoxxroot <luizhenrique.ambc@gmail.com>
2026-02-08 12:17:56 -0400

    Update config.php

--- web/config.php
+++ web/config.php
@@ -6,7 +6,7 @@ define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
 define('DB_PORT', getenv('DB_PORT') ?: '3306');
 define('DB_NAME', getenv('DB_NAME') ?: 'botblaze');
 define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: 'Vidaloka2026@');
+define('DB_PASS', getenv('DB_PASS') ?: '');
 
 // App
 define('APP_NAME', 'BotBlaze');
